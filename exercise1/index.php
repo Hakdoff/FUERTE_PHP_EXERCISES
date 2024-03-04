05902582f43d6d91bff0b60a6ff21492e573f2b2
Erika Fuerte <erikafuerte09@gmail.com>
2024-03-04 10:32:23 +0800

    "Modified Exercise 1"

--- exercise1/index.php
+++ exercise1/index.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
-    class Exercise_1 {
-        public function hello() {
-            echo "Hello World!";
-
-        }
+class Exercise_1 
+{
+    public function hello() 
+    {
+        echo "Hello, World!";
     }
+}
 
 $world = new Exercise_1;
 $world->hello();
-?>
