157c773e40c316d14bfc4d5beef7470c6ff5e341
Erika Fuerte <erikafuerte09@gmail.com>
2024-03-05 14:09:47 +0800

    Updated Exercise 6

--- exercise6/ArrayController.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include 'ArrayModel.php';
-
-$numbers = new ArrayManipulation;
-$ranNum = $numbers->numberSorting();
--- exercise6/index.php
+++ exercise6/index.php
@@ -1,1 +1,43 @@
-<?php include 'arrayView.php';
+<?php 
+
+class ArrayModel 
+{
+
+    public function numberSorting() 
+    {
+        $random = range(0, 100);
+        shuffle($random);
+        $ran = array_slice($random, 0,10);
+        $rand = implode(' ,', $ran );
+        echo "Array to be sorted: " . $rand;
+        echo "<br><br>";
+        $randomAsc = array_slice($random, 0,10);
+        sort($randomAsc);
+        $ranAsc = implode(' ,', $randomAsc );
+        echo "Ascending Order: " .$ranAsc;
+        echo "<br><br>";
+        $randomAsc = array_slice($random, 0,10);
+        sort($randomAsc);
+        $ranAsc = implode(' ,', $randomAsc );
+        echo "Descending Order: " .$ranAsc;
+    }  
+}
+
+$numbers = new ArrayModel;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Manipulation</title>
+    <link rel="stylesheet" href="ArrayStyle.css"/>
+</head>
+<body>
+    <div class="array">
+    <h1>Array Manipulation</h1>
+    <h4><?php $ranNum = $numbers->numberSorting(); ?></h4><br>
+    </div>    
+</body>
+</html>
