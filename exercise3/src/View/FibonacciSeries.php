02981cb8dc3820d4b9e8dcf735f157c21922c6d3
Erika Fuerte <erikafuerte09@gmail.com>
2024-03-07 10:07:41 +0800

    PR for Exercise 3 - v2

---
+++ exercise3/src/View/FibonacciSeries.php
@@ -0,0 +1,24 @@
+<?php use src\Controller\FibonacciController; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="FibonacciStyle.css">
+    <title>Fibonacci Sequence</title>  
+</head>
+<body>
+	<?php $print = new FibonacciController; 
+	?>
+    <div class="fibonacci"><br>
+    	<h1>Fibonacci Series</h1>
+		<form method="POST" action="">
+		Enter a number: <input type="number" name="num" required>
+		<br> <br>
+		<button class="submit" type="submit">Submit</button>
+		</form>
+		<h4 ><?php $print->getSeries(); ?></h4>
+	</div>
+</body>
+</html>
